c1496e6a3baf875784d0704fb06b749eef0e8a3f
david <david11sone@gmail.com>
2018-05-12 02:13:39 +0530

    lll

--- public/ng/directives/login/login.controller.js
+++ public/ng/directives/login/login.controller.js
@@ -1,11 +1,20 @@
 
-var app = angular.module("dIApp").controller("LoginController", function ($scope,$state) {
+var app = angular.module("dIApp").controller("LoginController", function ($scope, $state) {
+    
+    $scope.isRegister = false;
+    $scope.isForget =false;
 
     $scope.login = function (username, password) {
         console.log(username);
         console.log(password);
 
         $state.go("home.dashboard");
+    }
+
+   
+    $scope.register = function () {
+        
+
     }
 
 });
