05a63afdbb3a1ddd9d4ec0fb0c3df8828757c238
david <david11sone@gmail.com>
2018-05-12 05:15:05 +0530

    klkll

---
+++ public/ng/custom-directives/toggleButton.directive.js
@@ -0,0 +1,21 @@
+var app = angular.module('dIApp');
+app.directive('toggleButton', ['$http', '$timeout', function ($http, $timeout) {
+    return {
+        restrict: 'E',
+        template: '<div class="well">< i class= "fa fa-toggle-on active" ng-if= "status == true" ng-click="changeStatus();"> </i><i class="fa fa-toggle-on fa-rotate-180 inactive" ng-if="status == false" ng-click="changeStatus();"> </i> </div> <pre>{{ status }}</pre>',
+        scope: {
+            label: '@',
+            isLoad: '='
+        },
+        link: function (scope, element, attrs) {
+
+
+        },
+        controller: function ($scope, $state) {
+
+
+
+
+        }
+    };
+}])
