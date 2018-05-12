9d7a4b800b6677ae0e8f82b3dbfce7eba21a8e23
david <david11sone@gmail.com>
2018-05-12 05:53:09 +0530

    kkkkkk

--- public/ng/custom-directives/toggleButton.directive.js
+++ public/ng/custom-directives/toggleButton.directive.js
@@ -2,7 +2,8 @@ var app = angular.module('dIApp');
 app.directive('toggleButton', ['$http', '$timeout', function ($http, $timeout) {
     return {
         restrict: 'E',
-        template: '<div class="well" ng-init="init()">< i class= "fa fa-toggle-on active" ng-if= "status == true" ng-click="changeStatus();"> </i><i class="fa fa-toggle-on fa-rotate-180 inactive" ng-if="status == false" ng-click="changeStatus();"> </i> </div> <pre>{{ status }}</pre>',
+        templateUrl: 'ng/custom-directives/toggleButton.tmpl.html',
+        replace: true,
         scope: {
             label: '@',
             isLoad: '='
@@ -13,12 +14,29 @@ app.directive('toggleButton', ['$http', '$timeout', function ($http, $timeout) {
         },
         controller: function ($scope, $state) {
 
-            $scope.init = function () {
-                $scope.status = true;
-            }
-
+            $scope.status = false;
             $scope.changeStatus = function () {
                 $scope.status = !$scope.status;
+                if ($scope.status) {
+                    $("#sidenav-header").hide();
+                    $("#sidenav-header-logo").show();
+                    $(".main-navbar").toggleClass("shrink");
+                    // $("#sub-page-wrapper").removeClass("page-wrapper");
+                    $("#sub-page-wrapper").addClass("no-page-wrapper");
+                    $("#sub-page-wrapper").addClass("no-page-wrapper-animate-hide");
+
+
+                    $("nav").addClass("no-nav-wrapper")
+                } else {
+                    $("#sidenav-header-logo").hide();
+                    $("#sidenav-header").show();
+                    $(".main-navbar").removeClass("shrink");
+                    $("#sub-page-wrapper").addClass("page-wrapper");
+                    $("nav").removeClass("no-nav-wrapper")
+                    $("#sub-page-wrapper").removeClass("no-page-wrapper");
+
+
+                }
             }
 
 
--- public/ng/directives/header/header.directive.js
+++ public/ng/directives/header/header.directive.js
@@ -14,44 +14,7 @@ app.directive("mainHeader", ['$compile', function ($compile) {
 
             }
 
-            $scope.toggleSidebar = function (e) {
-                $scope.isToggle = !$scope.isToggle;
-                if ($scope.isToggle) {
-                    $("#sidenav-header").hide();
-                    $("#sidenav-header-logo").show();
-                    $(".main-navbar").toggleClass("shrink");
-                    // $("#sub-page-wrapper").removeClass("page-wrapper");
-                    $("#sub-page-wrapper").addClass("no-page-wrapper");
-                    $("#sub-page-wrapper").addClass("no-page-wrapper-animate-hide");
-                    
-                    
-                    $("nav").addClass("no-nav-wrapper")
-                } else {
-                    $("#sidenav-header-logo").hide();
-                    $("#sidenav-header").show();
-                    $(".main-navbar").removeClass("shrink");
-                    $("#sub-page-wrapper").addClass("page-wrapper");
-                    $("nav").removeClass("no-nav-wrapper")
-                    $("#sub-page-wrapper").removeClass("no-page-wrapper");
-                    
-
-                }
-                var w = $(window).outerWidth();
-                var h = $(window).outerHeight();
-                // e.preventDefault(),
-
-
-                    // (w > 1194) ? ($(".main-navbar").toggleClass("shrink"),
-                    //     //$(".page-wrapper").toggleClass("no-page-wrapper"),
-
-                    //     $("nav").toggleClass("no-nav-wrapper")
-                    // ) : (
-
-                    //         $(".main-navbar").toggleClass("show-sm"),
-                    //         $(".page-wrapper").toggleClass("active-sm")
-                    //     )
-
-            }
+           
 
         }
 
