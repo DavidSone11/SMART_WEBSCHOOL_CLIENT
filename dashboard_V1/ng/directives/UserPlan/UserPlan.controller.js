5c0d089839c2240fc33303200961cfbfda0fac52
david <david11sone@gmail.com>
2018-05-14 00:32:16 +0530

    ssssss

--- dashboard_V1/ng/directives/UserPlan/UserPlan.controller.js
+++ dashboard_V1/ng/directives/UserPlan/UserPlan.controller.js
@@ -1,23 +1,26 @@
 
 var app = angular.module("dIApp");
-app.controller("UserPlanController", ['$scope', '$timeout', '$http','$sce', function ($scope, $timeout, $http,$sce) {
+app.controller("UserPlanController", ['$scope', '$timeout', '$http', '$sce', function ($scope, $timeout, $http, $sce) {
     $scope.userplans = [];
     $scope.isloading = true;
 
-    $scope.userPlanUrl = {src:"http://localhost:4000/api/v1/userPlan/getPlans?callback=JSON_CALLBACK&limit=5&page=1&order=planName"};
-    $scope.trustSrc = function(src) {
+    $scope.userPlanUrl = { src: "http://localhost:4000/api/v1/userPlan/getPlans?callback=JSON_CALLBACK&limit=5&page=1&order=planName" };
+    $scope.usersPlanUrl ="http://localhost:4000/api/v1/userPlan/getPlans?callback=JSON_CALLBACK&limit=5&page=1&order=planName" ;
+    $scope.trustSrc = function (src) {
         return $sce.trustAsResourceUrl(src);
-      }
+    }
 
     $timeout(function () {
-        $http.get('http://localhost:4000/api/v1/userPlan/getPlans').then(function (response) {
+        $http.get('http://localhost:4000/api/v1/userPlan/getPlans').then(function successCallback(response) {
             $scope.userplans = response.data.results;
             $scope.isloading = false;
         });
 
-        // $http.jsonp($scope.userPlanUrl).then(function (response) {
-        //     $scope.data = response;
-        // });
+        $http.jsonp($scope.usersPlanUrl).then(function (response) {
+            $scope.data = response;
+        }, function errorCallback(response) {
+            // ko
+        });
 
 
 
