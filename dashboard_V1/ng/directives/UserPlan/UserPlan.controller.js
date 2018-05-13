1259006f7dbeb0f7a4df112f49493aa0a4e4bdbe
david <david11sone@gmail.com>
2018-05-13 23:12:25 +0530

    dsdsad

--- dashboard_V1/ng/directives/UserPlan/UserPlan.controller.js
+++ dashboard_V1/ng/directives/UserPlan/UserPlan.controller.js
@@ -1,12 +1,12 @@
 
 var app = angular.module("dIApp");
 app.controller("UserPlanController",  ['$scope','$timeout','$http',function($scope,$timeout,$http) {
-    $scope.users = [];
+    $scope.userplans = [];
     $scope.isloading = true;
 
     $timeout(function () {
         $http.get('http://localhost:4000/api/v1/userPlan/getPlans').then(function (response) {
-            $scope.users = response.data;
+            $scope.userplans = response.data.results;
             $scope.isloading = false;
         });
     }, 2000)
